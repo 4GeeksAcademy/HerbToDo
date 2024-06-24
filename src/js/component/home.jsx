dd4d524d935b44bfd582a59182ef18a73c84b439
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-06-24 21:56:37 +0000

    Finished TodoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	
+	const handleDelete = (index) => {
+		const updatedTodos = todos.filter((_, i) => i !== index);
+		setTodos(updatedTodos);
+	};
+
+	const handleEdit = (index, newValue) => {
+		setTodos((prevTodos) => {
+		  const updatedTodos = [...prevTodos];  // Deep copy using spread syntax
+		  updatedTodos[index] = newValue;
+		  return updatedTodos;
+		});
+	  };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todo {inputValue}</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e) => { 
+							if (e.key === "Enter" ) { 
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+						}}
+						placeholder="What do you need to do"
+					/>
+				</li>
+				{todos.map((todo, index) => (
+					<li key={index}>
+						<input
+							type="text"
+							value={todo}
+							onChange={(e) => handleEdit(index, e.target.value)}
+						/>
+						<i className="fas fa-trash-alt" onClick={() => handleDelete(index)}></i>
+					</li>
+				))}
+			</ul>
+			<div>{todos.length} tasks</div>
 		</div>
 	);
 };
